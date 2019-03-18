3c8c2b2e3ed200a86ea24b23afad65b6fdcbf973
Shengjie <liushengchieh@gmail.com>
2019-03-18 15:03:21 +0800

    测试

--- NullPointerTest/NullPointer.py
+++ NullPointerTest/NullPointer.py
@@ -7,29 +7,36 @@ import threading
 
 PATH = sys.path[0]
 #先判断设备是否连接
-os.popen("adb wait-for-device") 
+os.popen("adb wait-for-device")
 
 #判断包名
 
 
 #遍历文件获得activities 的值
 def CheckNullPoint():
-#Mac 下开启 
-#    f = open(PATH+ "/" + "activities.txt","r")  
-#windows 下开启
-    f = open(PATH+ "\\" + "activities.txt","r")
+#Mac 下开启
+    # f = open(PATH+ "/" + "activities.txt","r")
+    f = open(PATH+ "/" + "Activity_List", "r")
     for line in f.readlines():
-#	print (line)
-        os.popen("adb shell am start -n com.cootek.smartinputv5/" + line)
-        print("adb shell am start -n com.com.cootek.smartinputv5/" + line)
-        time.sleep(3)
+        os.popen("adb shell am start -n com.flashlight.brightest.beacon.torch/" + line)
+    print("adb shell am start -n com.flashlight.brightest.beacon.torch/" + line)
+    time.sleep(3)
     f.close()
+
+#windows 下开启
+    # f = open(PATH+ "\\" + "activities.txt","r")
+#     for line in f.readlines():
+# #	print (line)
+#         os.popen("adb shell am start -n com.cootek.smartinputv5/" + line)
+#         print("adb shell am start -n com.com.cootek.smartinputv5/" + line)
+#         time.sleep(3)
+#     f.close()
 
 def CatLog():
 #Mac 下的命令行
-#    os.popen("adb logcat >> ~/Desktop/TouchPal_nullpoint.log")
+   os.popen("adb logcat >> ~/Desktop/TouchPal_nullpoint.log")
 #windows 下的命令行
-    os.popen("adb logcat >> D:\\1TouchPal_nullpoint.log")
+    # os.popen("adb logcat >> D:\\1TouchPal_nullpoint.log")
 
 t2 = threading.Thread(target=CatLog)
 t1 = threading.Thread(target=CheckNullPoint)
