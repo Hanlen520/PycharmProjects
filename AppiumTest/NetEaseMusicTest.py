8a315bb3b9441dcb0dc999ba88b8a78d40628cd8
Shengjie <liushengchieh@gmail.com>
2019-04-24 15:55:27 +0800

    [new file]网易云音乐每日积分

---
+++ AppiumTest/NetEaseMusicTest.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author  : Shengjie.Liu
+# @Time    : 2019-04-24 15:39
+# @File    : NetEaseMusicTest.py
+# @Desc    : 
+
+import unittest
+from appium import webdriver
+
+
+class SimpleNetEaseMusicTest(unittest.TestCase):
+    def setUp(self):
+        # to-do:创建初始条件，封装成方法调用
+        pass
+
+    def tearDown(self):
+        # to-do:case结束后还原操作
+        pass
+
+
+class SimpleNetEaseMusicCase(SimpleNetEaseMusicTest):
+    # to-do:1、填写账号密码进入应用2、点击获取积分
+    pass
